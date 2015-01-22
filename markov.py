e92f683991b0f5f03de1bbecbb5e22a80a7bb582
Hackbright Student <info@hackbrightacademy.com>
2015-01-22 12:55:37 -0800

    Added output conditioning to eventually work with twitter and to generally clean up.

--- markov.py
+++ markov.py
@@ -3,6 +3,8 @@
 import sys
 import random
 
+MAX_TWITTER_LENGTH = 140
+
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
@@ -10,11 +12,11 @@ def make_chains(corpus):
     markov_keys = []
     markov_dict = {}
 
-
     for l in corpus:
-        line = l.rstrip()
+        line = l.strip()
         words = line.split(" ")
         for word in words:
+            word = word.lower()
             markov_list.append(word)
 
     for i in range(0, len(markov_list)-2):
@@ -26,31 +28,59 @@ def make_chains(corpus):
     return markov_dict
 
 
+    # markov_dict(i, i+1) = markov_dict.get(i, i+1, []) + i+2
+
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
+
+    # """Create markov list.
+
+    # jhkh kjgfh kjfdgh kjfdhgkj hdfkjghskjdfh ksdjh fkjdsh fkjsd hfkjsdh fksdjh file_to_open
+    # dsfkjsdhfjkdshfkjsdhfjksd.
+    # """
+
     current_key = random.choice(chains.keys())
     s = current_key[0] + " " + current_key[1]
-    count = 0
-    while current_key in chains and count < 150:
+    #count = 0
+    while current_key in chains:
         #print current_key
         next_word = random.choice(chains[current_key])
         #print next_word
         s = s + " " + next_word
         current_key = (current_key[-1], next_word)
-        count = count + 1
+        #count = count + 1
     return s
+
+def condition_text(string):
+    s = string[0:140]
+    if "!" in s or "?" in s or "." in s:
+        exclam = s.rfind("!")
+        question = s.rfind("?")
+        period = s.rfind(".")
+        punc = max([exclam, question, period])
+        s = s[0:punc+1]
+    else:
+        space = s.rfind(" ")
+        s = s[0:space] + "."
+    return s
+
+
+
 
 def main():
     args = sys.argv
 
- 
-    input_text = open(raw_input("Enter file to open: "))
+    script, file_to_open = args
 
+ 
+    #input_text = open(raw_input("Enter file to open: "))
+    input_text = open(file_to_open)
     chain_dict = make_chains(input_text)
     random_text = make_text(chain_dict)
     input_text.close()
-    print random_text 
+    #print random_text[0:140]
+    print condition_text(random_text)
 
 if __name__ == "__main__":
     main()
